cd46abe1764c6a838b0361dc3c978c9f3494a5d7
Kyathi Sudheer <kyathi.koilada@gmail.com>
2023-02-04 14:52:56 +0530

    Added table

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,81 +1,70 @@
 import React, { useState } from 'react';
-import { css } from '@emotion/css';
 
-const Portfolio = () => {
+function StockTable() {
   const [stocks, setStocks] = useState([]);
+  const [symbol, setSymbol] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [buySell, setBuySell] = useState('Buy');
 
-  const addStock = (symbol, quantity) => {
-    setStocks([...stocks, { symbol, quantity }]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStocks([...stocks, { symbol, quantity, price, buySell }]);
+    setSymbol('');
+    setQuantity(0);
+    setPrice(0);
+    setBuySell('Buy');
   };
-
-  const buyStock = (symbol, quantity) => {
-    setStocks(stocks.map((stock) => {
-      if (stock.symbol === symbol) {
-        return { ...stock, quantity: stock.quantity + quantity };
-      }
-      return stock;
-    }));
-  };
-
-  const sellStock = (symbol, quantity) => {
-    setStocks(stocks.map((stock) => {
-      if (stock.symbol === symbol) {
-        return { ...stock, quantity: stock.quantity - quantity };
-      }
-      return stock;
-    }));
-  };
-
-  const portfolioStyles = css`
-    padding: 20px;
-    text-align: center;
-
-    h1 {
-      font-size: 2em;
-      margin-bottom: 20px;
-    }
-
-    ul {
-      list-style: none;
-      padding: 0;
-      margin: 0;
-
-      li {
-        margin-bottom: 10px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-      }
-
-      button {
-        padding: 10px 20px;
-        background-color: lightgray;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 1em;
-        &:hover {
-          background-color: gray;
-        }
-      }
-    }
-  `;
 
   return (
-    <div className={portfolioStyles}>
-      <h1>Portfolio</h1>
-      <button onClick={() => addStock('AAPL', 100)}>Add Stock</button>
-      <ul>
-        {stocks.map((stock, index) => (
-          <li key={index}>
-            {stock.symbol} - {stock.quantity} shares
-            <button onClick={() => buyStock(stock.symbol, 100)}>Buy</button>
-            <button onClick={() => sellStock(stock.symbol, 100)}>Sell</button>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={symbol}
+          onChange={(e) => setSymbol(e.target.value)}
+          placeholder="Stock symbol"
+        />
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          placeholder="Quantity"
+        />
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Price"
+        />
+        <select value={buySell} onChange={(e) => setBuySell(e.target.value)}>
+          <option value="Buy">Buy</option>
+          <option value="Sell">Sell</option>
+        </select>
+        <button type="submit">Add stock</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Buy/Sell</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocks.map((stock, index) => (
+            <tr key={index}>
+              <td>{stock.symbol}</td>
+              <td>{stock.quantity}</td>
+              <td>{stock.price}</td>
+              <td>{stock.buySell}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-};
+}
 
-export default Portfolio;
+export default StockTable;
