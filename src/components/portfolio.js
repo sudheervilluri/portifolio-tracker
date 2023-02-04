ca4f0096b7cb1c0caa2682ff332e144b10e06a29
Kyathi Sudheer <kyathi.koilada@gmail.com>
2023-02-04 13:40:56 +0530

    Add tracker

---
+++ src/components/portfolio.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+const Portfolio = () => {
+  const [stocks, setStocks] = useState([]);
+
+  const addStock = (symbol, quantity) => {
+    setStocks([...stocks, { symbol, quantity }]);
+  };
+
+  const buyStock = (symbol, quantity) => {
+    setStocks(stocks.map((stock) => {
+      if (stock.symbol === symbol) {
+        return { ...stock, quantity: stock.quantity + quantity };
+      }
+      return stock;
+    }));
+  };
+
+  const sellStock = (symbol, quantity) => {
+    setStocks(stocks.map((stock) => {
+      if (stock.symbol === symbol) {
+        return { ...stock, quantity: stock.quantity - quantity };
+      }
+      return stock;
+    }));
+  };
+
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <button onClick={() => addStock('AAPL', 100)}>Add Stock</button>
+      <ul>
+        {stocks.map((stock, index) => (
+          <li key={index}>
+            {stock.symbol} - {stock.quantity} shares
+            <button onClick={() => buyStock(stock.symbol, 100)}>Buy</button>
+            <button onClick={() => sellStock(stock.symbol, 100)}>Sell</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Portfolio;
